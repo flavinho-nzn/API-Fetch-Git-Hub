d5728c3ac4c9e4d3575a9c15316967d5078f6fda
Flavio de jesus <flavinhorift@gmail.com>
2025-01-10 21:12:33 -0300

    Primeiro commit

---
+++ src/scripts/index.js
@@ -0,0 +1,55 @@
+import { getUser } from './service/user.js'
+import { getRepositories} from './service/repositories.js'
+
+
+import { user } from './objects/user.js'
+import { screen } from './objects/screen.js'
+
+document.getElementById('btn-search'). addEventListener('click', () => {
+    const userName = document.getElementById('input-search').value
+    if(validateEmptyInput(userName)) return
+    getUserData(userName)
+})
+
+
+
+document.getElementById('input-search'). addEventListener('keyup', (e) => {
+    const userName = e.target.value
+    const key = e.which || e.keyCode
+    const isEnterKeyPressed = key === 13
+
+    if(isEnterKeyPressed){
+        if(validateEmptyInput(userName)) return
+        getUserData(userName)
+    }
+})
+
+function validateEmptyInput(userName){
+    if(userName.length === 0){
+        alert('Preencha o campo com o nome do usúario do GitHub')
+        return true
+    }
+}
+
+
+async function getUserData(userName) {
+    try {
+        const userResponse = await getUser(userName);
+
+        if (userResponse.message === "Not found") {
+            screen.renderNotFound();
+            return;
+        }
+
+        const repositoriesResponse = await getRepositories(userName);
+
+        user.setInfo(userResponse);
+        user.setRepositories(repositoriesResponse);
+
+        screen.renderUser(user);
+    } catch (error) {
+        console.error("Erro ao obter dados do usuário:", error);
+        screen.renderNotFound(); // Pode renderizar mensagem de erro genérica
+    }
+}
+
